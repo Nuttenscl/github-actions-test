5f52a72a0e8d6f2f8f3f2d8bff2a917974372c94
Nuttenscl <Nuttenscl@users.noreply.github.com>
2020-10-16 11:10:28 +0200

    Update circles.py

--- circles.py
+++ circles.py
@@ -4,6 +4,6 @@ from math import pi
 def circle_area(r):
     if type(r) not in [int, float]:
         raise TypeError("The radius must be a non-negative real numbers.")
-    if r < 0:
-        raise ValueError("The radius cannot be negative.")
+    #if r < 0:
+    #    raise ValueError("The radius cannot be negative.")
     return pi * (r ** 2)
