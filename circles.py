d35b6c4c1a6942fdf3f277181f850871bc4ba0ae
Georgios Karakostas <karakostas.georgios@gmail.com>
2020-10-16 08:08:12 +0200

    Removed all conditions

--- circles.py
+++ circles.py
@@ -2,8 +2,8 @@ from math import pi
 
 
 def circle_area(r):
-    if type(r) not in [int, float]:
-        raise TypeError("The radius must be a non-negative real numbers.")
-    if r < 0:
-        raise ValueError("The radius cannot be negative.")
+    # if type(r) not in [int, float]:
+    #     raise TypeError("The radius must be a non-negative real numbers.")
+    # if r < 0:
+    #     raise ValueError("The radius cannot be negative.")
     return pi * (r ** 2)
